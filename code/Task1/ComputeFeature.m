609b259ea898b68fee4fad593072cbe0557c8974
Michal Staniaszek <m.staniaszek@gmail.com>
2014-04-27 17:06:37 +0200

    ComputeFeature made prettier

--- code/Task1/ComputeFeature.m
+++ code/Task1/ComputeFeature.m
@@ -1,27 +1,25 @@
 function [fs] = ComputeFeature(ii_ims, ftype)
+
 fn = ftype(1);
-x = ftype(2);
-y = ftype(3);
-w = ftype(4);
-h = ftype(5);
 
 if (fn > 4 || fn < 1)
     error('Error: Unknown feature type %d', ftype(1))
 end
 
+% convert to cell array so that we can pass the vector as comma separated
+% parameters
+fcell = num2cell(ftype(2:end));
+% define function cell array so we can call a different function based on
+% the feature type we want
+features = {@(im, ftr)(FeatureTypeI(im,fcell{:})),...
+     @(im, ftr)(FeatureTypeII(im,fcell{:})),...
+     @(im, ftr)(FeatureTypeIII(im,fcell{:})),...
+     @(im, ftr)(FeatureTypeIV(im,fcell{:}))};
+% one number for each image
 fs = zeros(1,size(ii_ims,1));
 
-for i=1:size(ii_ims,1)
-    switch fn
-        case 1
-            fs(i) = FeatureTypeI(ii_ims{i},x,y,w,h)
-        case 2
-            fs(i) = FeatureTypeII(ii_ims{i},x,y,w,h)
-        case 3
-            fs(i) = FeatureTypeIII(ii_ims{i},x,y,w,h)
-        otherwise
-            fs(i) = FeatureTypeIV(ii_ims{i},x,y,w,h)
-    end
+for i=1:size(ii_ims,1) 
+    fs(i) = features{fn}(ii_ims{i}, ftype);
 end
 
 end
