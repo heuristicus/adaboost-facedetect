358fae7e7e8bb567b05f2d0917ab2a6404d71372
Michal Staniaszek <m.staniaszek@gmail.com>
2014-05-08 17:43:39 +0200

    Updated the boosting algorithm to fix some issues
    Mostly matrix dimension issues

--- code/Task2/BoostingAlg.m
+++ code/Task2/BoostingAlg.m
@@ -1,24 +1,47 @@
 function Cparams = BoostingAlg(Fdata, NFdata, FTdata, T)
 %BOOSTINGALG 
 fmat=FTdata.fmat;
-all_ftypes=FTdata.all_ftypes;
-ii_imsFace=Fdata.ii_ims;
+all_ftypes=FTdata.all_ftypes; % nfeatures x 5
+ii_imsFace=Fdata.ii_ims; % nimages x W*H
 ii_imsNonF=NFdata.ii_ims;
+
+% Define the number of face and non-face images. These determine which of
+% the labels are positive or negative examples.
 Flength=size(ii_imsFace,1);
 Nlength=size(ii_imsNonF,1);
-weights=zeros(Flength+Nlength,1);
+
+% Define the weights for each image in the training set
+weights=ones(Flength+Nlength,1);
+% weights = weights*(1/sum(weights));
 weights(1:Nlength)=1/(2*Nlength);
-weights(Nlength:end)=1/(2*Flength);
+weights(Nlength+1:end)=1/(2*Flength);
+
+% The labels for each training image are defined by whether they come from
+% the face or non-face data. Face data is positive.
 labels=[zeros(Nlength,1);ones(Flength,1)];
+
+% Predefine the vectors used to store the parameters which are computed by
+% each weak classifier. Their length corresponds to the number of features
+% being used.
 theta = zeros(1,size(all_ftypes,1));
 p = zeros(1,size(all_ftypes,1));
 error = zeros(1,size(all_ftypes,1));
+
+% The parameters which are computed by the optimal weak classifier in each
+% iteration.
 Cparams.alphas = zeros(T,1);
 Cparams.thetas = zeros(T,3);
-ii_imFaces = [ii_imsNonF, ii_imsFace];
+
+% Combine the integral image matrices of the non-face and face images into
+% a single one to allow for easier computation of the feature vector.
+ii_imFaces = [ii_imsNonF; ii_imsFace];
+
 for t=1:T
+    tic
+    fprintf('Choosing weak classifier %d\n', t)
     weights=weights/sum(weights);
     for j = 1:size(all_ftypes,1)
+        j
         fVec=fmat(:,j);
         fs=ii_imFaces*fVec;
         [theta(j), p(j), error(j)]=LearnWeakClassifier(weights, fs, labels);
@@ -26,13 +49,15 @@ for t=1:T
     [error_t, min_index] = min(error);
     Cparams.thetas(t,:) = [min_index, theta(min_index), p(min_index)];
     beta = error_t/(1-error_t);
+    betavec = ones(size(labels)) * beta;
     Cparams.alphas(t) = log(1/beta);
-        
     
     fVec=fmat(:,min_index);
     fs=ii_imFaces*fVec;
-    classification=p(min_index)*fs<p(min_index)*theta;
-    weights = weights * beta^(1-abs(classification-labels))
+    classification=p(min_index)*fs<p(min_index)*theta(min_index);
+    weights = weights .* (betavec .^(1-abs(classification-labels)));
+    time=toc;
+    fprintf('Time taken: %f\n', time);
 end
 
 
--- code/Task2/Task2.m
+++ code/Task2/Task2.m
@@ -1,5 +1,5 @@
 %% Compute Histogram of feature responses
-fNumber=15000;
+fNumber=998;
 Fdata = load('data/FaceData.mat');
 NFdata = load('data/NonFaceData.mat');
 FTdata = load('data/FeaturesToUse.mat');
@@ -14,8 +14,6 @@ fs=ii_ims*fVec;
 Nhist=Nelem./sum(Nelem);
 Fhist=elem./sum(elem);
 
-
-
 %% Testing Weak Classifier Initial Parameters
 % Run previous section to test
 [ weights, fs, class ] = GetWeakClassifierParams(ii_ims, Nii_ims, fVec);
@@ -24,7 +22,7 @@ Fhist=elem./sum(elem);
 close all
 figure
 hold on
-plot(Ncenter,Nhist,'b');
+plot(Ncenter,Nhist,'b');    
 plot(center,Fhist,'r');
 line([theta,theta],[0,max(Nhist)], 'color', 'k');
 title('fVec ',fNumber);
@@ -32,12 +30,29 @@ hold off
 
 %% Plot feature types
 
-fpic = MakeFeaturePic([1, 5, 5, 5, 5], 19, 19);
+fpic = MakeFeaturePic([4, 5, 5, 5, 5], 19, 19);
+figure
 imagesc(fpic)
 colormap('gray')
 
 all_ftypes = FTdata.all_ftypes;
 cpic = MakeClassifierPic(all_ftypes, [5192, 12765], [1.8725,1.467],[1,-1],19,19);
+figure
 imagesc(cpic)
 colormap('gray')
 
+%% Debug 1
+% check the results of the boosting algorithm
+
+dinfo6 = load('data/DebugInfo/debuginfo6.mat');
+T = dinfo6.T;
+Cparams = BoostingAlg(Fdata, NFdata, FTdata, T);
+sum(abs(dinfo6.alphas - Cparams.alphas)>eps)
+sum(abs(dinfo6.Thetas(:) - Cparams.thetas(:))>eps)
+
+ourtheta=Cparams.thetas(:)
+theirtheta=dinfo6.Thetas(:)
+ouralpha = Cparams.alphas
+theiralpha = dinfo6.alphas
+thetadiff = Cparams.thetas(:) - dinfo6.Thetas(:)
+alphadiff = Cparams.alphas - dinfo6.alphas
